38451f730c79b04fc85fd7dcae76c7f5d5d4e067
VictorMerino2002 <merino.rodriguez.victor@gmail.com>
2024-09-24 23:36:07 +0200

    img resize module added

--- frontend/src/components/Editor.jsx
+++ frontend/src/components/Editor.jsx
@@ -1,46 +1,65 @@
 /* eslint-disable react/prop-types */
-import ReactQuill from 'react-quill'
+import ReactQuill, { Quill } from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
+import ResizeModule from "@majintd/quill-image-resize";
+
+Quill.register("modules/resize", ResizeModule);
 
 export const Editor = ({editorContent, setEditorContent,readOnly}) => {
 
-  const handleEditorChange = (content) => {
-    console.log(editorContent)
-    setEditorContent(content)
-  }
+    const handleEditorChange = (content) => {
+        if (readOnly) return
+        console.log(content)
+        setEditorContent(content)
+    }
+
+
+    const formats = [
+        'bold', 'italic', 'underline', 'strike', 
+        'blockquote', 'link', 'image', 'video', 
+        'list', 'bullet', 'check', 'indent', 
+        'size', 'header', 'color', 'background', 
+        'font', 'align'
+    ]
+
   let modules = {
     toolbar: [
-      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+      ['bold', 'italic', 'underline', 'strike'],
       ['blockquote'],
       ['link', 'image', 'video'],
                
       [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
-      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent
+      [{ 'indent': '-1'}, { 'indent': '+1' }],
   
+      [{ 'size': ['small', false, 'large', 'huge'] }],
       [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
   
-      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      [{ 'color': [] }, { 'background': [] }], 
       [{ 'font': [] }],
       [{ 'align': [] }]
-      ]
+      ],
+      resize: {
+        locale: {},
+      }
   }
 
   if (readOnly) {
     modules = {
-      toolbar : false
+      toolbar : false,
+      resize: {locale : {}}
     }
   }
 
-
   return (
     <div className='Editor'>
-      <ReactQuill 
-        value={editorContent}
-        onChange={handleEditorChange}
-        theme='snow'
-        readOnly={readOnly}
-        modules={modules}
-      />
+        <ReactQuill
+          value={editorContent}
+          onChange={handleEditorChange}
+          theme='snow'
+          readOnly={readOnly}
+          modules={modules}
+          formats={formats}
+        />
     </div>
   )
 }
